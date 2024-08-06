5892bf275982240510e2046f1a44ef7b572eca76
PetrikAleksey <47816260+PetrikAleksey@users.noreply.github.com>
2024-08-06 15:20:05 +0300

    added assert in FramesTest

--- src/main/java/org/example/selenide/tests/FramesTest.java
+++ src/main/java/org/example/selenide/tests/FramesTest.java
@@ -3,8 +3,11 @@ package org.example.selenide.tests;
 import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.WindowType;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class FramesTest extends BaseTest{
+
+    private final SoftAssert softAssert = new SoftAssert();
 
     @Test
     public void frameTest(){
@@ -17,7 +20,13 @@ public class FramesTest extends BaseTest{
             Selenide.switchTo().defaultContent();
         }
 
-        System.out.println("Test");
+        for (int i = 0; i <= 4; i++) {
+            Selenide.switchTo().frame(i);
+            softAssert.assertEquals("Это фрейм " + (i + 1), framesPage.getFrameByIndex(i).getAttribute("value"));
+            Selenide.switchTo().defaultContent();
+        }
+
+        softAssert.assertAll();
     }
 
 }
